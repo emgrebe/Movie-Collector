dfa571710e3c44aecd540446b0b3b5469e0ecb0f
Emily Landgrebe <emilandgrebe@gmail.com>
2019-11-19 17:38:26 -0600

    created Watching model with date and times

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+TIMES = (
+  ('M', 'Morning'),
+  ('A', 'Afternoon'),
+  ('E', 'Evening')
+)
+
 class Movie(models.Model):
   title = models.CharField(max_length=100)
   year = models.IntegerField()
@@ -11,3 +17,16 @@ class Movie(models.Model):
   
   def get_absolute_url(self):
     return reverse('detail', kwargs={ 'movie_id': self.id })
+
+class Watching(models.Model):
+  date = models.DateField('watch date')
+  time = models.CharField(
+    max_length=1,
+    choices=TIMES,
+    default=TIMES[0][0]
+  )
+
+  movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_time_display()} on {self.date}"
