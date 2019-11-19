b5dcf6b00277e2ad896d351fefd21fa6546bd678
Emily Landgrebe <emilandgrebe@gmail.com>
2019-11-18 22:51:35 -0600

    create card for movies

--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,24 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+class Movie:
+  def __init__(self, title, year, actors, description):
+    self.title = title
+    self.year = year
+    self.actors = actors
+    self.description = description
+
+movies = [
+  Movie('Star Wars: A New Hope', 1977, 'Mark Hamill (Luke Skywalker), Harrison Ford (Han Solo), Carrie Fisher (Princess Leia), Alec Guinness (Obi-Wan Kenobi)', 'The first Star Wars movie made'),
+  Movie('Star Wars: The Empire Strikes Back', 1980, 'Mark Hamill (Luke Skywalker), Harrison Ford (Han Solo), Carrie Fisher (Princess Leia), David Prowse (Darth Vader)', 'The second Star Wars movie made'),
+  Movie('Star Wars: Return of The Jedi', 1983, 'Mark Hamill (Luke Skywalker), Harrison Ford (Han Solo), Carrie Fisher (Princess Leia), David Prowse (Darth Vader)', 'The third Star Wars movie made')
+]
+
 def home(request):
   return HttpResponse('<h1>Hello</h1>')
 
 def about(request):
   return render(request, 'about.html')
+
+def movies_index(request):
+  return render(request, 'movies/index.html', { 'movies': movies })
